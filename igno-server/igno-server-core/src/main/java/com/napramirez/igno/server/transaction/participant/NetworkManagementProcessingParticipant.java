5cbd8d08c2489a074d7a8fca394103d3620d43d1
Nap Ramirez <napramirez@gmail.com>
2012-09-13 15:20:26 +0800

    implemented the following: - network management processing participant - stored procedure calling participant - authorization processing participant - response and reference codes

---
+++ igno-server/igno-server-core/src/main/java/com/napramirez/igno/server/transaction/participant/NetworkManagementProcessingParticipant.java
@@ -0,0 +1,64 @@
+package com.napramirez.igno.server.transaction.participant;
+
+import java.io.Serializable;
+
+import org.jpos.iso.ISOMsg;
+import org.jpos.transaction.TransactionParticipant;
+import org.jpos.util.Log;
+
+import com.napramirez.igno.server.common.constants.NetworkManagementInformationCode;
+import com.napramirez.igno.server.common.constants.ResponseCode;
+import com.napramirez.igno.server.transaction.TransactionContext;
+
+/**
+ * NetworkManagementProcessingParticipant
+ * 
+ * @author <a href="mailto:napramirez@gmail.com">Nap Ramirez</a>
+ */
+public class NetworkManagementProcessingParticipant
+    extends Log
+    implements TransactionParticipant
+{
+    public int prepare( long id, Serializable context )
+    {
+        return PREPARED | READONLY;
+    }
+
+    public void commit( long id, Serializable context )
+    {
+        try
+        {
+            TransactionContext ctx = (TransactionContext) context;
+            ISOMsg request = (ISOMsg) ctx.get( "request" );
+
+            String netManCode = request.getString( 70 );
+
+            if ( NetworkManagementInformationCode.LOGON.toString().equals( netManCode ) )
+            {
+                info( "Log on" );
+            }
+            else if ( NetworkManagementInformationCode.LOGOFF.toString().equals( netManCode ) )
+            {
+                info( "Log off" );
+            }
+            else if ( NetworkManagementInformationCode.ECHO_TEST.toString().equals( netManCode ) )
+            {
+                info( "Echo Test" );
+            }
+
+            ISOMsg response = (ISOMsg) request.clone();
+            response.setResponseMTI();
+            response.set( 39, ResponseCode.POS.APPROVED.toString() );
+
+            ctx.put( "response", response );
+        }
+        catch ( Exception e )
+        {
+            error( e );
+        }
+    }
+
+    public void abort( long id, Serializable context )
+    {
+    }
+}
