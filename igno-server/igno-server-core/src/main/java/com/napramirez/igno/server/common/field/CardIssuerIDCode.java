0ab1f7d85a633ea3d5f22ee0ef8592d335fa834a
Nap Ramirez <napramirez@gmail.com>
2012-09-25 21:24:44 +0800

    implemented field helpers for fields 095 and 122p

---
+++ igno-server/igno-server-core/src/main/java/com/napramirez/igno/server/common/field/CardIssuerIDCode.java
@@ -0,0 +1,48 @@
+package com.napramirez.igno.server.common.field;
+
+/**
+ * CardIssuerIDCode - Field 122 in FIS ISO Specifications
+ *
+ * POS
+ *
+ * @author <a href="mailto:napramirez@gmail.com">Nap Ramirez</a>
+ */
+public class CardIssuerIDCode
+{
+    private static final int FIELD_LENGTH = 14;
+
+    private String fieldLengthIndicator;
+
+    private String cardIssuerId;
+
+    public CardIssuerIDCode( String fieldStringValue )
+    {
+        if ( fieldStringValue == null || fieldStringValue.length() != FIELD_LENGTH )
+        {
+            throw new IllegalArgumentException( "Card Issuer ID Code field is invalid!" );
+        }
+
+        fieldLengthIndicator = fieldStringValue.substring( 0, 3 );
+        cardIssuerId = fieldStringValue.substring( 3 );
+    }
+
+    public String getFieldLengthIndicator()
+    {
+        return fieldLengthIndicator;
+    }
+
+    public void setFieldLengthIndicator( String fieldLengthIndicator )
+    {
+        this.fieldLengthIndicator = fieldLengthIndicator;
+    }
+
+    public String getCardIssuerId()
+    {
+        return cardIssuerId;
+    }
+
+    public void setCardIssuerId( String cardIssuerId )
+    {
+        this.cardIssuerId = cardIssuerId;
+    }
+}
