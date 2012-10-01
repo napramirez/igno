21f3a954fad1870cd6e13a29bf901477f2576937
Nap Ramirez <napramirez@gmail.com>
2012-10-01 10:52:35 +0800

    moved logic to prepare() in order to provide a more graceful failure

--- igno-server/igno-server-core/src/main/java/com/napramirez/igno/server/transaction/participant/AuthorizationProcessingParticipant.java
+++ igno-server/igno-server-core/src/main/java/com/napramirez/igno/server/transaction/participant/AuthorizationProcessingParticipant.java
@@ -15,6 +15,7 @@ import org.jpos.iso.ISOMsg;
 import org.jpos.transaction.TransactionParticipant;
 import org.jpos.util.Log;
 import org.jpos.util.NameRegistrar;
+import org.jpos.util.NameRegistrar.NotFoundException;
 
 import com.napramirez.igno.server.message.field.Track2Data;
 import com.napramirez.igno.server.message.field.constants.ResponseCode;
@@ -34,18 +35,81 @@ public class AuthorizationProcessingParticipant
 
     public int prepare( long id, Serializable context )
     {
+        long startTime = System.currentTimeMillis();
+
         try
         {
             DataSource ds = (DataSource) NameRegistrar.get( CONNECTION_POOL_NAME );
             conn = ds.getConnection();
             cs = conn.prepareCall( storedProcedure, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE );
         }
-        catch ( Exception e )
+        catch ( NotFoundException e )
         {
+            error( e );
+            return ABORTED;
+        }
+        catch ( SQLException e )
+        {
+            error( e );
             return ABORTED;
         }
 
+        TransactionContext ctx = (TransactionContext) context;
+        ISOMsg request = (ISOMsg) ctx.get( "request" );
+
+        Long pan = getPAN( request );
+        double amount = Double.valueOf( request.getString( 4 ) );
+
+        double newBalance;
+        boolean isAuthorized;
+        try
+        {
+            cs.setLong( 1, pan );
+            cs.setDouble( 2, amount );
+            cs.registerOutParameter( 3, Types.NUMERIC );
+            cs.registerOutParameter( 4, Types.BOOLEAN );
+
+            cs.execute();
+
+            newBalance = cs.getDouble( 3 );
+            isAuthorized = cs.getBoolean( 4 );
+
+            cs.close();
+            conn.close();
+        }
+        catch ( SQLException e )
+        {
+            error( e );
+            return ABORTED;
+        }
+
+        ISOMsg response = (ISOMsg) request.clone();
+        try
+        {
+            response.setResponseMTI();
+
+            response.set( 38, "A00000" );
+            response.set( 39, isAuthorized ? ResponseCode.POS.APPROVED.toString() : ResponseCode.POS.DECLINE.toString() );
+        }
+        catch ( ISOException e )
+        {
+            error( e );
+            return ABORTED;
+        }
+
+        ctx.put( "response", response );
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        info( "pg_authorize( " + pan + ", " + amount + ", " + newBalance + ", " + isAuthorized + " ), " + elapsedTime
+            + "ms" );
+
         return PREPARED;
+    }
+
+    public void commit( long id, Serializable context )
+    {
     }
 
     public void abort( long id, Serializable context )
@@ -68,65 +132,23 @@ public class AuthorizationProcessingParticipant
         }
     }
 
-    public void commit( long id, Serializable context )
+    private long getPAN( ISOMsg message )
     {
-        long startTime = System.currentTimeMillis();
-
-        try
+        String panString = message.getString( 2 );
+        if ( StringUtils.isNotBlank( panString ) )
         {
-            TransactionContext ctx = (TransactionContext) context;
-            ISOMsg request = (ISOMsg) ctx.get( "request" );
-
-            Long pan = null;
-
-            String panString = request.getString( 2 );
-            if ( StringUtils.isNotBlank( panString ) )
+            return Long.valueOf( panString );
+        }
+        else
+        {
+            String track2DataString = message.getString( 35 );
+            if ( StringUtils.isNotBlank( track2DataString ) )
             {
-                pan = Long.valueOf( panString );
+                Track2Data track2Data = new Track2Data( track2DataString );
+                return Long.valueOf( track2Data.getPan() );
             }
-            else
-            {
-                String track2DataString = request.getString( 35 );
-                if ( StringUtils.isNotBlank( track2DataString ) )
-                {
-                    Track2Data track2Data = new Track2Data( track2DataString );
-                    pan = Long.valueOf( track2Data.getPan() );
-                }
-            }
-
-            double amount = Double.valueOf( request.getString( 4 ) );
-
-            cs.setLong( 1, pan );
-            cs.setDouble( 2, amount );
-            cs.registerOutParameter( 3, Types.NUMERIC );
-            cs.registerOutParameter( 4, Types.BOOLEAN );
-
-            cs.execute();
-
-            double newBalance = cs.getDouble( 3 );
-            boolean isAuthorized = cs.getBoolean( 4 );
-
-            ISOMsg response = (ISOMsg) request.clone();
-            response.setResponseMTI();
-
-            response.set( 38, "A00000" );
-            response.set( 39, isAuthorized ? ResponseCode.POS.APPROVED.toString() : ResponseCode.POS.DECLINE.toString() );
-
-            ctx.put( "response", response );
-
-            long endTime = System.currentTimeMillis();
-            long elapsedTime = endTime - startTime;
-
-            info( "pg_authorize( " + pan + ", " + amount + ", " + newBalance + ", " + isAuthorized + " ), "
-                + elapsedTime + "ms" );
         }
-        catch ( ISOException e )
-        {
-            throw new RuntimeException( e );
-        }
-        catch ( SQLException e )
-        {
-            throw new RuntimeException( e );
-        }
+
+        return 0;
     }
 }
